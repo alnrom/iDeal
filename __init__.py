cb0675ebbec2a4eea587097df31d6e577a99816a
alanrom <romeroa@MacBook-Air-de-Jesus.local>
2022-09-07 23:06:19 -0500

    more files

---
+++ __init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET KEY'] = '1234'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+
+    from .models import User, Note
+
+    create_database(app)
+    
+    return app
+
+
+def create_database(app):
+    if not path.exists('templates/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
